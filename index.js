50adc50b68403820bcba2776f7ddd1192b527a16
Chuck Smith <chuck@eclecticsaddlebag.com>
2019-08-07 14:52:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 function theBeatlesPlay(musicans, instruments) {
   const arr = []
   
-  for (let i = 0, x = musicans.length; i < x, i++)
+  for (let i = 0, x = musicans.length; i < x, i++) {
+    arr.push
+  }
   
 }
